a10689c80998e9a7d08ad0eb8d47db9e7e925a66
Richie Lee <richie@bzzzt.io>
2020-09-13 12:18:25 +0100

    add format test

--- runeatest/nunit.py
+++ runeatest/nunit.py
@@ -1,6 +1,14 @@
+from runeatest import pyspark
+
+
 def get_nunit_header():
-    nunit_header = '<test-results name="##notebookPath##" total="##total##" date="##getdate##" time="##gettime##">\n<environment nunit-version="2.6.0.12035" clr-version="2.0.50727.4963" os-version="Microsoft Windows NT 6.1.7600.0" platform="Win32NT" cwd="C:\\Program Files\\NUnit 2.6\\bin\\" machine-name="dummymachine" user="dummyuser" user-domain="dummy"/>\n<culture-info current-culture="en-US" current-uiculture="en-US"/>'
+    context = pyspark.getcontext()
+    nunit_header = '<test-results name="##name##" total="##total##" date="##getdate##" time="##gettime##">\n<environment nunit-version="2.6.0.12035" clr-version="2.0.50727.4963" os-version="Microsoft Windows NT 6.1.7600.0" platform="Win32NT" cwd="C:\\Program Files\\NUnit 2.6\\bin\\" machine-name="dummymachine" user="dummyuser" user-domain="dummy"/>\n<culture-info current-culture="en-US" current-uiculture="en-US"/>'
+    nunit_header = nunit_header.replace("##name##", context["notebook_path"])
     return nunit_header
+
+
+# $xmlHeader.Replace("##notebookPath##",$notebookPath).Replace('##total##',$notebookTestResults.Count).Replace('##getdate##',(Get-Date -Format yyyy-MM-dd)).Replace('##gettime##',(Get-Date -Format hh:mm:ss))
 
 
 def get_nunit_footer():
--- tests/test_nunit.py
+++ tests/test_nunit.py
@@ -1,9 +1,15 @@
 import pytest
+import json
 from runeatest import nunit
+from runeatest import pyspark
 
 
-def test_get_nunit_header():
-    expected = '<test-results name="##notebookPath##" total="##total##" date="##getdate##" time="##gettime##">\n<environment nunit-version="2.6.0.12035" clr-version="2.0.50727.4963" os-version="Microsoft Windows NT 6.1.7600.0" platform="Win32NT" cwd="C:\\Program Files\\NUnit 2.6\\bin\\" machine-name="dummymachine" user="dummyuser" user-domain="dummy"/>\n<culture-info current-culture="en-US" current-uiculture="en-US"/>'
+def test_get_nunit_header(mocker):
+    x = '{"notebook_path":"/Users/lorem.ipsum@fake.io/runeatest"}'
+    context = json.loads(x)
+    mocker.patch("runeatest.pyspark.getcontext", return_value=context)
+    print(context["notebook_path"])
+    expected = '<test-results name="/Users/lorem.ipsum@fake.io/runeatest" total="##total##" date="##getdate##" time="##gettime##">\n<environment nunit-version="2.6.0.12035" clr-version="2.0.50727.4963" os-version="Microsoft Windows NT 6.1.7600.0" platform="Win32NT" cwd="C:\\Program Files\\NUnit 2.6\\bin\\" machine-name="dummymachine" user="dummyuser" user-domain="dummy"/>\n<culture-info current-culture="en-US" current-uiculture="en-US"/>'
     actual = nunit.get_nunit_header()
     assert expected == actual
 
@@ -11,3 +17,4 @@ def test_get_nunit_header():
 def test_get_nunit_footer():
     expected = "</results>\n</test-suite>\n</test-results>"
     actual = nunit.get_nunit_footer()
+    assert expected == actual
