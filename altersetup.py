af28d216e2c7dc817719e3f1b1c5b5f897b16dc2
Richie Lee <richie@bzzzt.io>
2020-09-16 20:21:26 +0100

    pass in arguments was out of range, silly.

--- altersetup.py
+++ altersetup.py
@@ -3,6 +3,6 @@ print(sys.argv)
 fin = open("setup-tokenised.py", "rt")
 fout = open("setup.py", "wt")
 for line in fin:
-  fout.write(line.replace('@@Major@@', sys.argv[2]).replace('@@Minor@@', sys.argv[3]).replace('@@Revision@@', sys.argv[4]))
+  fout.write(line.replace('@@Major@@', sys.argv[1]).replace('@@Minor@@', sys.argv[2]).replace('@@Revision@@', sys.argv[3]))
 fin.close()
 fout.close()
