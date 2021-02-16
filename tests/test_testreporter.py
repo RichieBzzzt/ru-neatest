a1ea9433d4d90cfb5da3fdbe6c5ecf570813d040
Richie Lee <richie@bzzzt.io>
2021-02-16 12:41:36 +0000

    formatting on save

--- tests/test_testreporter.py
+++ tests/test_testreporter.py
@@ -7,7 +7,10 @@ def test_add_all_passed_test_cases():
     actual = []
     actual.append(testreporter.add_testcase("test name", True))
     actual.append(testreporter.add_testcase("test name 2", True))
-    expected = [{'test': 'test name', 'issuccess': 'True', 'result': 'success'}, {'test': 'test name 2', 'issuccess': 'True', 'result': 'success'}]
+    expected = [
+        {"test": "test name", "issuccess": "True", "result": "success"},
+        {"test": "test name 2", "issuccess": "True", "result": "success"},
+    ]
     assert expected == actual
 
 
@@ -16,7 +19,7 @@ def test_add_passed_test_case():
 
     actual = []
     actual.append(testreporter.add_testcase("test name", True))
-    expected = [{'test': 'test name', 'issuccess': 'True', 'result': 'success'}]
+    expected = [{"test": "test name", "issuccess": "True", "result": "success"}]
     assert expected == actual
 
 
@@ -25,7 +28,7 @@ def test_add_failed_test_case():
 
     actual = []
     actual.append(testreporter.add_testcase("test name", False))
-    expected = [{'test': 'test name', 'issuccess': 'False', 'result': 'failure'}]
+    expected = [{"test": "test name", "issuccess": "False", "result": "failure"}]
     assert expected == actual
 
 
@@ -33,10 +36,19 @@ def test_add_all_failed_test_cases():
     from runeatest import testreporter
 
     actual_test_add_all_failed_test_cases = []
-    actual_test_add_all_failed_test_cases.append(testreporter.add_testcase("test name", False))
-    actual_test_add_all_failed_test_cases.append(testreporter.add_testcase("test name 2", False))
-    expected_test_add_all_failed_test_cases = [{'test': 'test name', 'issuccess': 'False', 'result': 'failure'}, {'test': 'test name 2', 'issuccess': 'False', 'result': 'failure'}]
-    assert expected_test_add_all_failed_test_cases == actual_test_add_all_failed_test_cases
+    actual_test_add_all_failed_test_cases.append(
+        testreporter.add_testcase("test name", False)
+    )
+    actual_test_add_all_failed_test_cases.append(
+        testreporter.add_testcase("test name 2", False)
+    )
+    expected_test_add_all_failed_test_cases = [
+        {"test": "test name", "issuccess": "False", "result": "failure"},
+        {"test": "test name 2", "issuccess": "False", "result": "failure"},
+    ]
+    assert (
+        expected_test_add_all_failed_test_cases == actual_test_add_all_failed_test_cases
+    )
 
 
 def test_add_one_passed_one_failed_test_cases():
@@ -45,9 +57,13 @@ def test_add_one_passed_one_failed_test_cases():
     actual = []
     actual.append(testreporter.add_testcase("test name", True))
     actual.append(testreporter.add_testcase("test name 2", False))
-    expected = [{'test': 'test name', 'issuccess': 'True', 'result': 'success'}, {'test': 'test name 2', 'issuccess': 'False', 'result': 'failure'}]
+    expected = [
+        {"test": "test name", "issuccess": "True", "result": "success"},
+        {"test": "test name 2", "issuccess": "False", "result": "failure"},
+    ]
     actual_string = str(actual)
     assert expected == actual
+
 
 def test_add_one_passed_one_failed_test_cases_to_string():
     from runeatest import testreporter
@@ -59,12 +75,22 @@ def test_add_one_passed_one_failed_test_cases_to_string():
     actual_string = str(actual)
     assert expected == actual_string
 
+
 def test_add_all_failed_test_cases_to_string():
     from runeatest import testreporter
 
     actual_test_add_all_failed_test_cases = []
-    actual_test_add_all_failed_test_cases.append(testreporter.add_testcase("test name", False))
-    actual_test_add_all_failed_test_cases.append(testreporter.add_testcase("test name 2", False))
+    actual_test_add_all_failed_test_cases.append(
+        testreporter.add_testcase("test name", False)
+    )
+    actual_test_add_all_failed_test_cases.append(
+        testreporter.add_testcase("test name 2", False)
+    )
     expected_test_add_all_failed_test_cases_string = "[{'test': 'test name', 'issuccess': 'False', 'result': 'failure'}, {'test': 'test name 2', 'issuccess': 'False', 'result': 'failure'}]"
-    actual_test_add_all_failed_test_cases_string = str(actual_test_add_all_failed_test_cases)
-    assert expected_test_add_all_failed_test_cases_string == actual_test_add_all_failed_test_cases_string
+    actual_test_add_all_failed_test_cases_string = str(
+        actual_test_add_all_failed_test_cases
+    )
+    assert (
+        expected_test_add_all_failed_test_cases_string
+        == actual_test_add_all_failed_test_cases_string
+    )
